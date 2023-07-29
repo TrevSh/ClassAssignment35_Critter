d4d0e444d135838c01ead50f5b39cf0845c2d96e
TrevSh <trevonshelby@outlook.com>
2023-07-29 02:39:39 -0500

    Added private variables and functions

--- ClassAssignment35_Critter/ClassAssignment35_Critter.cpp
+++ ClassAssignment35_Critter/ClassAssignment35_Critter.cpp
@@ -5,15 +5,34 @@
 
 class Critter {         //class definition -- defines a new type, Critter.
 public:
-    int m_Hunger;       //data member
     void Greet();       //member function
     Critter(int hunger);//Constructor prototype
+    int GetHunger() const;
+    void SetHunger(int hunger);
+private:
+    int m_Hunger;       //private data member
 };
 
-//Constructor for Critter. Is called each time a Critter item is instantiated.
-Critter::Critter(int hunger) {      
+//Constructor for Critter. Is called each time a Critter item is instantiated. Must have same name as class.
+Critter::Critter(int hunger) :      //Using member initialization to give hunger a value. Colon starts initialization followed by brackets after variables are assigned values.
+    m_Hunger(hunger)                
+{
     std::cout << "A critter! A critter has been born!" << "\n";
-    m_Hunger = hunger;
+}
+
+//returns a private variable. In this case, hunger. Const because we dont want to change the value here.
+int Critter::GetHunger() const {
+    return m_Hunger;
+}
+
+//Used to modify a private variable.
+void Critter::SetHunger(int hunger) {
+    if (hunger < 0) {
+        std::cout << "Illegal. Cannot be lower than 0. Im calling the cops.";
+    }
+    else {
+        m_Hunger = hunger;
+    }
 }
 
 void Critter::Greet() {     //Member function definition
@@ -23,7 +42,15 @@ void Critter::Greet() {     //Member function definition
 int main()
 {
     Critter c1(4);          //Creating a critter using the constructor. 
-    c1.Greet();
+
+    //We use GetHunger() because you cannot directly access a private variable.
+    std::cout << "Calling GetHunger(): " << c1.GetHunger() << "\n\n";
+
+    std::cout << "Calling SetHunger() with -1. \n";
+    c1.SetHunger(-1);
+    std::cout << "Calling SetHunger() with 9. \n";
+    c1.SetHunger(9);
+    std::cout << "Calling GetHunger(): " << c1.GetHunger() << "\n\n";
 
     system("pause");
 }
